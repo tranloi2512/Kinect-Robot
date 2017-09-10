dd5f5cdb4cca28e65b973ef628ebb9218c4b185d
tranloi2512 <tranloi2512@gmail.com>
2017-09-10 23:37:47 +0700

    Add files via upload

---
+++ webrtc_video_text/main.js
@@ -0,0 +1,240 @@
+const socket = io('https://stream3005.herokuapp.com/');
+
+$('#div-chat').hide();
+
+let customConfig;
+
+$.ajax({
+  url: "https://service.xirsys.com/ice",
+  data: {
+    ident: "vanpho",
+    secret: "2b1c2dfe-4374-11e7-bd72-5a790223a9ce",
+    domain: "vanpho93.github.io",
+    application: "default",
+    room: "default",
+    secure: 1
+  },
+  success: function (data, status) {
+    // data.d is where the iceServers object lives
+    customConfig = data.d;
+    console.log(customConfig);
+  },
+  async: false
+});
+
+socket.on('DANH_SACH_ONLINE', arrUserInfo => {
+    $('#div-chat').show();
+    $('#div-dang-ky').hide();
+
+    arrUserInfo.forEach(user => {
+        const { ten, peerId } = user;
+        $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
+    });
+
+    socket.on('CO_NGUOI_DUNG_MOI', user => {
+        const { ten, peerId } = user;
+        $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
+    });
+
+    socket.on('AI_DO_NGAT_KET_NOI', peerId => {
+        $(`#${peerId}`).remove();
+    });
+});
+
+socket.on('DANG_KY_THAT_BAT', () => alert('Vui long chon username khac!'));
+
+
+function openStream() {
+    const config = { audio: false, video: true };
+    return navigator.mediaDevices.getUserMedia(config);
+}
+
+function playStream(idVideoTag, stream) {
+    const video = document.getElementById(idVideoTag);
+    video.srcObject = stream;
+    video.play();
+}
+
+// openStream()
+// .then(stream => playStream('localStream', stream));
+
+const peer = new Peer({ 
+    key: 'peerjs', 
+    host: 'mypeer3005.herokuapp.com', 
+    secure: true, 
+    port: 443, 
+   // debug: 3,	//
+	config: customConfig 
+	
+});
+
+peer.on('open', id => {
+	//
+	$('#pid').text(id);
+	console.log('My ID is: '+id);
+	//
+    $('#my-peer').append(id);
+    $('#btnSignUp').click(() => {
+        const username = $('#txtUsername').val();
+        socket.emit('NGUOI_DUNG_DANG_KY', { ten: username, peerId: id });
+    });
+});
+
+// Await connections from others
+peer.on('connection', connect);
+peer.on('error', function(err) {
+console.log(err);
+})
+
+
+// Handle a connection object.
+function connect(c) {
+  // Handle a chat connection.
+  if (c.label === 'chat') {	
+  console.log('C label = chat');
+    var chatbox = $('<div></div>').addClass('connection').addClass('active').attr('id', c.peer);
+    var header = $('<h1></h1>').html('Chat with <strong>' + c.peer + '</strong>');
+    var messages = $('<div><em>Peer connected.</em></div>').addClass('messages');
+    chatbox.append(header);
+    chatbox.append(messages);
+	console.log("Message to send is: "+messages);
+ 
+    // Select connection handler.
+    chatbox.on('click', function() {
+	console.log('chatbox click event');
+      if ($(this).attr('class').indexOf('active') === -1) {
+        $(this).addClass('active');
+      } else {
+        $(this).removeClass('active');
+      }
+    });
+    $('.filler').hide();
+    $('#connections').append(chatbox);
+    c.on('data', function(data) {
+      messages.append('<div><span class="peer">' + c.peer + '</span>: ' + data +
+        '</div>');
+        });
+        c.on('close', function() {
+          alert(c.peer + ' has left the chat.');
+          chatbox.remove();
+          if ($('.connection').length === 0) {
+            $('.filler').show();
+          }
+          delete connectedPeers[c.peer];
+        });
+  } 
+  connectedPeers[c.peer] = 1;
+}
+
+var connectedPeers = {};
+
+$(document).ready(function() {
+
+  function doNothing(e){
+    e.preventDefault();
+    e.stopPropagation();
+  }
+  // Connect to a peer
+  $('#connect').click(function() {		//Button Connect Click event
+   // var requestedPeer = $('#rid').val();
+	var requestedPeer = remoteID;
+	console.log('Remote ID is:'+requestedPeer);
+    if (!connectedPeers[requestedPeer]) {
+      // Create 2 connections, one labelled chat and another labelled file.
+      var c = peer.connect(requestedPeer, {
+        label: 'chat',
+        serialization: 'none',
+        metadata: {message: 'hi i want to chat with you!'}
+      });
+      c.on('open', function() {
+        connect(c);
+      });
+      c.on('error', function(err) { alert(err); });
+    
+    }
+    connectedPeers[requestedPeer] = 1;
+  });
+  // Close a connection.
+  $('#close').click(function() {		// Close Connect Event
+    eachActiveConnection(function(c) {
+      c.close();
+    });
+  });
+  // Send a chat message to all active connections.
+  $('#send').submit(function(e) {		//Send Message to Remote Peer 
+    e.preventDefault();
+    // For each active connection, send the message.
+    var msg = $('#text').val();
+	console.log('<< Send event >> Message to send is: '+msg);
+    eachActiveConnection(function(c, $c) {
+      if (c.label === 'chat') {
+        c.send(msg);
+        $c.find('.messages').append('<div><span class="you">You: </span>' + msg
+          + '</div>');
+      }
+    });
+    $('#text').val('');
+    $('#text').focus();
+  });
+  // Goes through each active peer and calls FN on its connections.
+  function eachActiveConnection(fn) {
+    var actives = $('.active');
+    var checkedIds = {};
+    actives.each(function() {
+      var peerId = $(this).attr('id');
+      if (!checkedIds[peerId]) {
+        var conns = peer.connections[peerId];
+        for (var i = 0, ii = conns.length; i < ii; i += 1) {
+          var conn = conns[i];
+          fn(conn, $(this));
+        }
+      }
+      checkedIds[peerId] = 1;
+    });
+  }
+  
+});
+
+// Make sure things clean up properly.
+window.onunload = window.onbeforeunload = function(e) {
+  if (!!peer && !peer.destroyed) {
+    peer.destroy();
+  }
+};
+
+
+//Caller
+$('#btnCall').click(() => {
+    const id = $('#remoteId').val();
+    openStream()
+    .then(stream => {
+        playStream('localStream', stream);
+        const call = peer.call(id, stream);
+        call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+    });
+});
+
+//Callee
+peer.on('call', call => {
+    openStream()
+    .then(stream => {
+        call.answer(stream);
+        playStream('localStream', stream);
+        call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+    });
+});
+var remoteID;
+$('#ulUser').on('click', 'li', function() {
+    const id = $(this).attr('id');
+    console.log('KHOA PHAM connect to oid: '+id);
+	remoteID=id;
+	//
+	
+	//
+    openStream()
+    .then(stream => {
+        playStream('localStream', stream);
+        const call = peer.call(id, stream);
+        call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+    });
+});
